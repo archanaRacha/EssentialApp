865d1554689f1ed4b80e50e4d18e25de82be2c81
archanaRacha <3062400+archanaRacha@users.noreply.github.com>
2024-10-19 17:33:32 +0530

    FeedImageDataLoaderWithFallbackComposite.loadImageData loads from primary loader first

--- AE-App/AE-AppTests/LocalWithRemoteFallbackFeedImageDataLoaderTests.swift
+++ AE-App/AE-AppTests/LocalWithRemoteFallbackFeedImageDataLoaderTests.swift
@@ -45,9 +45,22 @@ final class FeedImageDataLoaderWithFallbackTests: XCTestCase {
         XCTAssertTrue(primaryLoader.loadedURLs.isEmpty, "Expected no loaded URLs in the primary loader")
         XCTAssertTrue(fallbackLoader.loadedURLs.isEmpty, "Expected no loaded URLs in the fallback loader")
     }
+    func test_loadImageData_loadsFromPrimaryLoaderFirst() {
+        let url = anyURL()
+        let primaryLoader = LoaderSpy()
+        let fallbackLoader = LoaderSpy()
+        let sut = FeedImageDataLoaderWithFallbackComposite(primaryImageDataLoader: primaryLoader, fallbackImageDataLoader: fallbackLoader)
+
+        _ = sut.loadImageData(from: url) { _ in }
+
+        XCTAssertEqual(primaryLoader.loadedURLs, [url], "Expected to load URL from primary loader")
+        XCTAssertTrue(fallbackLoader.loadedURLs.isEmpty, "Expected no loaded URLs in the fallback loader")
+    }
     
 // MARK: - Helpers
-
+    private func anyURL() -> URL {
+        return URL(string: "http://a-url.com")!
+    }
     private class LoaderSpy: FeedImageDataLoader {
         private var messages = [(url: URL, completion: (FeedImageDataLoader.Result) -> Void)]()
 
