7d3dfea2319e0ae10e100577155e341070bb766c
archanaRacha <3062400+archanaRacha@users.noreply.github.com>
2024-10-19 16:58:44 +0530

    move  FeedLoaderWithFallbackComposite to production

--- AE-App/AE-AppTests/RemoteWithLocalFallbackFeedLoaderTests.swift
+++ AE-App/AE-AppTests/RemoteWithLocalFallbackFeedLoaderTests.swift
@@ -7,25 +7,8 @@
 
 import XCTest
 import AE_Feed
+import AE_App
 
-class FeedLoaderWithFallbackComposite : FeedLoader{
-    private let primary : FeedLoader
-    private let fallback: FeedLoader
-    init(primary:FeedLoader,fallback:FeedLoader){
-        self.primary = primary
-        self.fallback = fallback
-    }
-    func load(completion:@escaping(FeedLoader.Result) -> Void){
-        primary.load {[weak self] result in
-            switch result {
-            case .success:
-                completion(result)
-            case .failure:
-                self?.fallback.load(completion: completion)
-            }
-        }
-    }
-}
 class FeedLoaderWithFallbackCompositeTests:XCTestCase{
     func test_load_deliversPrimaryFeedOnPrimarySuccess() {
         let primaryFeed = uniqueFeed()
