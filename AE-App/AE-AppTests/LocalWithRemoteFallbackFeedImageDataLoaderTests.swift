2c1a11165390ea3c122efe10cebcd29f28c4b3f6
archanaRacha <3062400+archanaRacha@users.noreply.github.com>
2024-10-20 17:16:42 +0530

    Move FeedImageDataLoaderWithFallbackComposite to production

--- AE-App/AE-AppTests/LocalWithRemoteFallbackFeedImageDataLoaderTests.swift
+++ AE-App/AE-AppTests/LocalWithRemoteFallbackFeedImageDataLoaderTests.swift
@@ -7,33 +7,7 @@
 
 import XCTest
 import AE_Feed
-
-private class FeedImageDataLoaderWithFallbackComposite : FeedImageDataLoader {
-    private let primaryImageDataLoader:FeedImageDataLoader
-    private let fallbackImageDataLoader:FeedImageDataLoader
-    init(primaryImageDataLoader: FeedImageDataLoader, fallbackImageDataLoader: FeedImageDataLoader) {
-        self.primaryImageDataLoader = primaryImageDataLoader
-        self.fallbackImageDataLoader = fallbackImageDataLoader
-    }
-    private class TaskWrapper : FeedImageDataLoaderTask{
-        var wrapped: FeedImageDataLoaderTask?
-        func cancel() {
-            wrapped?.cancel()
-        }
-    }
-    func loadImageData(from url: URL, completion: @escaping ((FeedImageDataLoader.Result) -> Void)) -> any AE_Feed.FeedImageDataLoaderTask {
-        let task = TaskWrapper()
-        task.wrapped = primaryImageDataLoader.loadImageData(from: url) {[weak self] result in
-            switch result{
-            case .success:
-                completion(result)
-            case .failure:
-                task.wrapped = self?.fallbackImageDataLoader.loadImageData(from: url, completion: completion)
-            }
-        }
-        return task
-    }
-}
+import AE_App
 
 final class FeedImageDataLoaderWithFallbackTests: XCTestCase {
     
