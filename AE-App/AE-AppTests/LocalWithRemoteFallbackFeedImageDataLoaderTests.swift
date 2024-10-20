9ec7eb78abd3b1e1096d659d63335afefb3f7bfb
archanaRacha <3062400+archanaRacha@users.noreply.github.com>
2024-10-20 15:37:27 +0530

    FeedImageDataLoaderWithFallbackComposite.loadImageData cancels fallback loader task on cancel after a primary loader failure

--- AE-App/AE-AppTests/LocalWithRemoteFallbackFeedImageDataLoaderTests.swift
+++ AE-App/AE-AppTests/LocalWithRemoteFallbackFeedImageDataLoaderTests.swift
@@ -76,7 +76,17 @@ final class FeedImageDataLoaderWithFallbackTests: XCTestCase {
         XCTAssertEqual(primaryLoader.cancelledURLs, [url], "Expected to cancel URL loading from primary loader")
         XCTAssertTrue(fallbackLoader.cancelledURLs.isEmpty, "Expected no cancelled URLs in the fallback loader")
     }
-    
+    func test_cancelLoadImageData_cancelsFallbackLoaderTaskAfterPrimaryLoaderFailure() {
+        let url = anyURL()
+        let (sut, primaryLoader, fallbackLoader) = makeSUT()
+
+        let task = sut.loadImageData(from: url) { _ in }
+        primaryLoader.complete(with: anyNSError())
+        task.cancel()
+
+        XCTAssertTrue(primaryLoader.cancelledURLs.isEmpty, "Expected no cancelled URLs in the primary loader")
+        XCTAssertEqual(fallbackLoader.cancelledURLs, [url], "Expected to cancel URL loading from fallback loader")
+    }
 // MARK: - Helpers
     private func anyNSError() -> NSError {
         return NSError(domain: "any error", code: 0)
