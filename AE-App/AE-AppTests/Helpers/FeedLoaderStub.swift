6cbe654af1542e588fa188f227e4d2d9da32fdff
archanaRacha <3062400+archanaRacha@users.noreply.github.com>
2024-10-21 21:23:49 +0530

    tract FeedLoaderStub into a shared scope to remove duplication

---
+++ AE-App/AE-AppTests/Helpers/FeedLoaderStub.swift
@@ -0,0 +1,21 @@
+//
+//  FeedLoaderStub.swift
+//  AE-AppTests
+//
+//  Created by archana racha on 21/10/24.
+//
+
+import Foundation
+import AE_Feed
+
+public class FeedLoaderStub : FeedLoader {
+    
+    private var result : FeedLoader.Result
+    
+    public init(result : FeedLoader.Result){
+        self.result = result
+    }
+    public func load(completion: @escaping (FeedLoader.Result) -> Void) {
+        completion(result)
+    }
+}
