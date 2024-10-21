0aa62fb6bcc21e8a2b3290a6d0b2443b773a8e05
archanaRacha <3062400+archanaRacha@users.noreply.github.com>
2024-10-21 22:57:34 +0530

    Extract FeedImageDataLoader test helpers into a shared protocol extension to remove duplication

--- AE-App/AE-AppTests/FeedImageDataLoaderWithFallbackCompositeTests.swift
+++ AE-App/AE-AppTests/FeedImageDataLoaderWithFallbackCompositeTests.swift
@@ -9,7 +9,7 @@ import XCTest
 import AE_Feed
 import AE_App
 
-final class FeedImageDataLoaderWithFallbackTests: XCTestCase {
+final class FeedImageDataLoaderWithFallbackCompositeTests: XCTestCase,FeedImageDataLoaderTestCase {
     
     func test_init_doesNotLoadImageData() {
         let primaryLoader = FeedImageDataLoaderSpy()
@@ -87,28 +87,7 @@ final class FeedImageDataLoaderWithFallbackTests: XCTestCase {
         })
     }
 // MARK: - Helpers
-    private func expect(_ sut: FeedImageDataLoader, toCompleteWith expectedResult: FeedImageDataLoader.Result, when action: () -> Void, file: StaticString = #file, line: UInt = #line) {
-        let exp = expectation(description: "Wait for load completion")
 
-        _ = sut.loadImageData(from: anyURL()) { receivedResult in
-            switch (receivedResult, expectedResult) {
-            case let (.success(receivedFeed), .success(expectedFeed)):
-                XCTAssertEqual(receivedFeed, expectedFeed, file: file, line: line)
-
-            case (.failure, .failure):
-                break
-
-            default:
-                XCTFail("Expected \(expectedResult), got \(receivedResult) instead", file: file, line: line)
-            }
-
-            exp.fulfill()
-        }
-
-        action()
-
-        wait(for: [exp], timeout: 1.0)
-    }
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedImageDataLoader, primary: FeedImageDataLoaderSpy, fallback: FeedImageDataLoaderSpy){
         
         let primaryLoader = FeedImageDataLoaderSpy()
