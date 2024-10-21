d09501d19d300bb9c1db1f8bc58094668552f984
archanaRacha <3062400+archanaRacha@users.noreply.github.com>
2024-10-21 21:52:29 +0530

    Replace if-try-statement with map

--- AE-App/AE-AppTests/FeedLoaderCacheDecoratorTests.swift
+++ AE-App/AE-AppTests/FeedLoaderCacheDecoratorTests.swift
@@ -23,10 +23,12 @@ final class FeedLoaderCacheDecorator:FeedLoader {
     }
     func load(completion:@escaping(FeedLoader.Result) -> Void){
         decoratee.load { [weak self] result in
-            if let feed = try? result.get() {
+            completion(result.map { feed in
+            
                 self?.cache.save(feed) { _ in }
-            }
-            completion(result)
+            
+                return feed
+            })
         }
     }
 }
