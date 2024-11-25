078d06cd490aad4e92253d39dfdd4c1b1464b403
archanaRacha <3062400+archanaRacha@users.noreply.github.com>
2024-11-25 10:07:06 +0530

    Reset cache in every test run to make sure tests run in a clean state… … and dont influence the result of other tests

--- AE-App/AE-AppUIAcceptenceTests/AE_AppUIAcceptenceTests.swift
+++ AE-App/AE-AppUIAcceptenceTests/AE_AppUIAcceptenceTests.swift
@@ -11,10 +11,33 @@ final class AE_AppUIAcceptenceTests: XCTestCase {
 
     func test_onLaunch_displayRemoteFeedWhenCustomerHasConnectivity(){
         let app = XCUIApplication()
+        app.launchArguments = ["-reset"]
         app.launch()
         let feedCells = app.cells.matching(identifier: "feed-image-cell")
         XCTAssertEqual(feedCells.count, 22)
         let firstImage = app.images.matching(identifier: "feed-image-view").firstMatch
         XCTAssertEqual(firstImage.exists, true)
+    }
+    func test_onLaunch_displayCacheRemoteFeedWhenCustomerHasNoConnectivity(){
+        let onlineApp = XCUIApplication()
+        onlineApp.launchArguments = ["-reset"]
+        onlineApp.launch()
+        
+        let offlineApp = XCUIApplication()
+        offlineApp.launchArguments = ["-connectivity","offline"]
+        offlineApp.launch()
+        
+        let cachedFeedCells = offlineApp.cells.matching(identifier: "feed-image-cell")
+        XCTAssertEqual(cachedFeedCells.count,22)
+        let firstCachedImage = offlineApp.images.matching(identifier: "feed-image-view").firstMatch
+        XCTAssertTrue(firstCachedImage.exists)
+    }
+    func test_onLaunch_displaysEmptyFeedWhenCustomerHasNoConnectivityAndNoCache(){
+        let app = XCUIApplication()
+        app.launchArguments = ["-reset","-connectivity","offline"]
+        app.launch()
+        
+        let feedCells = app.cells.matching(identifier: "feed-image-cell")
+        XCTAssertEqual(feedCells.count,0)
     }
 }
