63e6023d61574772136aad359c266319a4d0af30
archanaRacha <3062400+archanaRacha@users.noreply.github.com>
2024-12-07 16:03:17 +0530

    remove unnecessary main storyboard - the initial setup happens in code.

---
+++ AE-App/AE-AppTests/Helpers/SceneDelegateTests.swift
@@ -0,0 +1,35 @@
+//
+//  SceneDelegateTests.swift
+//  AE-AppTests
+//
+//  Created by archana racha on 07/12/24.
+//
+
+import XCTest
+import AEFeediOS
+@testable import AE_App
+
+final class SceneDelegateTests: XCTestCase {
+    func test_configureWindow_setsWindowAsKeyAndVisible(){
+        let window = UIWindow()
+        let sut = SceneDelegate()
+        sut.window = window
+        sut.configure()
+//        XCTAssertTrue(window.isKeyWindow,"Expected window to be the key window")
+        XCTAssertFalse(window.isHidden,"Expected window to be the visible")
+        
+    }
+//    func test_sceneWillConnectToSession_configuresRootViewController(){
+//        let sut = SceneDelegate()
+//        sut.window = UIWindow()
+//        sut.configure()
+//        
+//        let root = sut.window?.rootViewController
+//        let rootNavigation = root as? UINavigationController
+//        let topController = rootNavigation?.topViewController
+//        
+//        XCTAssertNotNil(rootNavigation,"Expected a navigation controller as root, got \(String(describing: root)) instead")
+//        XCTAssertTrue(topController is FeedViewController,"Expected a feed controller as top view controller,got\(String(describing:topController)) instead")
+//        
+//    }
+}
---
+++ AE-App/AEFeedAPI/AEFeedAPI.h
@@ -0,0 +1,18 @@
+//
+//  AEFeedAPI.h
+//  AEFeedAPI
+//
+//  Created by archana racha on 03/12/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AEFeedAPI.
+FOUNDATION_EXPORT double AEFeedAPIVersionNumber;
+
+//! Project version string for AEFeedAPI.
+FOUNDATION_EXPORT const unsigned char AEFeedAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AEFeedAPI/PublicHeader.h>
+
+
